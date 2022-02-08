d067667eb00ce1f663a4fbf76904e28788bb8623
James Darnley <jdarnley@obe.tv>
2022-02-08 17:59:59 +0100

    upipe: add ubuf_super

---
+++ lib/upipe/ubuf_super.c
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2022 Open Broadcast Systems Ltd.
+ *
+ * Authors: James Darnley
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject
+ * to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/** @file
+ * @short something
+ */
+
+#include "upipe/ubase.h"
+#include "upipe/urefcount.h"
+#include "upipe/ubuf.h"
+#include "upipe/ubuf_super.h"
+
+struct ubuf_super {
+    struct ubuf **buf_b, **buf_p, **buf_s;
+    struct ubuf ubuf;
+};
+
+struct ubuf_super_mgr {
+    struct ubuf_mgr **mgr_b, **mgr_p, **mgr_s;
+    uint8_t num_mgr_b, num_mgr_p, num_mgr_s;
+    struct urefcount refcount;
+    struct ubuf_mgr mgr;
+};
+
+UBASE_FROM_TO(ubuf_super, ubuf, ubuf, ubuf)
+
+UBASE_FROM_TO(ubuf_super_mgr, ubuf_mgr, ubuf_mgr, mgr)
+UBASE_FROM_TO(ubuf_super_mgr, urefcount, urefcount, refcount)
+
+static void ubuf_super_mgr_free(struct urefcount *urefcount)
+{
+    struct ubuf_super_mgr *ctx = ubuf_super_mgr_from_urefcount(urefcount);
+    urefcount_clean(urefcount);
+    free(ctx);
+}
+
+struct ubuf_mgr *ubuf_super_mgr_alloc(void)
+{
+    struct ubuf_super_mgr *ctx = malloc(sizeof(struct ubuf_super_mgr));
+    if (unlikely(ctx == NULL))
+        return NULL;
+
+    ctx->mgr_b = ctx->mgr_p = ctx->mgr_s = NULL;
+    ctx->num_mgr_b = ctx->num_mgr_p = ctx->num_mgr_s = 0;
+
+    urefcount_init(ubuf_super_mgr_to_urefcount(ctx), ubuf_super_mgr_free);
+
+    struct ubuf_mgr *mgr = ubuf_super_mgr_to_ubuf_mgr(ctx);
+    mgr->signature = UBUF_SUPER;
+
+    return mgr;
+}
